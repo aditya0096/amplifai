bef16b6aeb860e90336437edbd71cb18e2038287
aditya0096 <adityajadhav0096@gmail.com>
2025-09-08 14:37:22 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/login");
+  }
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -24,7 +31,6 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
@@ -38,6 +44,7 @@ export default function Home() {
               alt="Vercel logomark"
               width={20}
               height={20}
+              priority
             />
             Deploy now
           </a>
@@ -101,3 +108,4 @@ export default function Home() {
     </div>
   );
 }
+
